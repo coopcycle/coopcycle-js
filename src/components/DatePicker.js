21ab14b7b4d2a1eb8ec0c7687b27d233d4892392
Atala <alois.guillope@gmail.com>
2017-10-02 17:09:44 +0200

    fix: all opening hours should be available for next day
    Fix #18

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -11,36 +11,48 @@ moment.locale('fr')
 
 class DatePicker extends Component {
 
-  onChangeDate(event) {
-    const { time } = this.props
+  constructor (props) {
+    super(props);
+
+    const { availabilities } = this.props;
+    const days = _.groupBy(availabilities, date => moment(date).format('YYYY-MM-DD'));
+    const dates = _.keys(days);
+    const availableTimes = days[_.first(dates)].map(date => moment(date).format('HH:mm'));
+
+    this.state = {
+      'availableTimes': availableTimes
+    };
+  }
+
+  onChangeDate(event, days) {
+    const { time } = this.props;
+    this.setState({ 'availableTimes': days[event.target.value].map(date => moment(date).format('HH:mm'))});
     this.props.actions.setDeliveryDate(event.target.value + ' ' + time + ':00')
   }
 
   onChangeTime(event) {
-    const { date } = this.props
+    const { date } = this.props;
     this.props.actions.setDeliveryDate(date + ' ' + event.target.value + ':00')
   }
 
   render() {
 
     const { availabilities } = this.props;
-
-    const days = _.groupBy(availabilities, date => moment(date).format('YYYY-MM-DD'))
-    const dates = _.keys(days)
-    const times = days[_.first(dates)].map(date => moment(date).format('HH:mm'))
+    const days = _.groupBy(availabilities, date => moment(date).format('YYYY-MM-DD'));
+    const dates = _.keys(days);
 
     return (
       <div className="row">
         <div className="col-sm-6">
-          <select value={ this.props.date } className="form-control" onChange={ this.onChangeDate.bind(this) }>
+          <select value={ this.props.date } className="form-control" onChange={ (evt) => this.onChangeDate(evt, days) }>
             { dates.map(date =>
               <option key={ date } value={ date }>{ moment(date).format('dddd DD MMM') }</option>
             ) }
           </select>
         </div>
         <div className="col-sm-6">
-          <select value={ this.props.time } className="form-control" onChange={ this.onChangeTime.bind(this) }>
-            { times.map(time =>
+          <select value={ this.props.time } className="form-control" onChange={ (evt) => this.onChangeTime(evt) }>
+            { this.state.availableTimes.map(time =>
               <option key={ time }>{ time }</option>
             ) }
           </select>
@@ -61,8 +73,6 @@ function mapStateToProps(state, props) {
     date = moment(state.deliveryDate).format('YYYY-MM-DD')
     time = moment(state.deliveryDate).format('HH:mm')
   }
-
-  const availabilities = state.restaurant.availabilities
 
   return {
     availabilities: state.restaurant.availabilities,
