c80cca20e0ee7687104c2704f98210d15696f88b
erwanledoux <lawanledoux@gmail.com>
2017-10-18 22:44:49 +0200

    added a description form in Address

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,17 +1,28 @@
-import React, { Component } from 'react';
-import { Panel } from 'react-bootstrap';
+import React, { Component } from 'react'
+import { ControlLabel,
+  FormControl,
+  FormGroup,
+  Panel
+} from 'react-bootstrap'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 class Address extends Component {
+  handleChange(event) {
+    this.setState({ [ event.target.name ]: event.target.value });
+  }
   render() {
     const title = (
       <h3>Livraison</h3>
     );
-
     return (
       <Panel header={ title }>
         { this.props.cartAddress ? this.props.cartAddress.streetAddress : '' }
+        <FormGroup>
+          <ControlLabel>Additional Information (digicode, etc...)</ControlLabel>
+          <FormControl name="description" type="description"
+            onChange={ this.handleChange.bind(this) } />
+        </FormGroup>
       </Panel>
     )
   }
